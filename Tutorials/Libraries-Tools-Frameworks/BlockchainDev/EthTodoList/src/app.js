ac4fb3b636841b5b91e529ac3c7fd31cdbe70aaa
Jerry Quintero <jerryq27@gmail.com>
2022-02-28 21:36:16 -0600

    Reached a new checkpoint in TodoList tutorial.

---
+++ Tutorials/Libraries-Tools-Frameworks/BlockchainDev/EthTodoList/src/app.js
@@ -0,0 +1,60 @@
+App = {
+    contracts: {},
+    load: async () => {
+        // Load app..
+        console.log("App loading...");
+        await App.loadWeb3();
+        await App.loadAccount();
+        await App.loadContract();
+        await App.render();
+    },
+    loadWeb3: async () => {
+        if(typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
+        } else {
+            window.alert("Please connect to Metamask.")
+        }
+        // Modern dapp browsers...
+        if(window.ethereum) {
+            window.web3 = new Web3(ethereum)
+            try {
+                // Request account access if needed
+                await ethereum.enable()
+                // Acccounts now exposed
+                web3.eth.sendTransaction({/* ... */})
+            } catch (error) {
+                // User denied account access...
+            }
+        }
+        // Legacy dapp browsers...
+        else if(window.web3) {
+            App.web3Provider = web3.currentProvider
+            window.web3 = new Web3(web3.currentProvider)
+            // Acccounts always exposed
+            web3.eth.sendTransaction({/* ... */})
+        }
+        // Non-dapp browsers...
+        else {
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+    },
+    loadAccount: async () => {
+        App.account = web3.eth.accounts[0];
+        console.log("Account: " + App.account);
+    },
+    loadContract: async () => {
+        const todoList = await $.getJSON('TodoList.json');
+        App.contracts.TodoList = TruffleContract(todoList);
+        App.contracts.TodoList.setProvider(App.web3Provider);
+
+        App.todoList = await App.contracts.TodoList.deployed();
+    },
+    render: async () => {
+        $("#account").html(App.account);
+    }
+}
+
+$(() => {
+    $(window).load(() => App.load())
+});
